8a5c0adfc0f8cd2f141b7411173a27be2bafde47
mohdAldabagh <56668595+mohdAldabagh@users.noreply.github.com>
2023-04-15 11:47:25 +0300

    Added TODO list

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  //TODO 18: Create call back function
+
+  const HomeScreen({
+    super.key,
+  });
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  //TODO 1: object from TextEditingController
+
+  //TODO 2: Variable to save the user Email
+
+  //TODO 4: Create a method to save Shared Preferences data
+
+  //TODO 7: Create a method to read Shared Preferences data
+
+  //TODO 8: Call the initState method
+
+  //TODO 9: Call the dispose method
+
   @override
   Widget build(BuildContext context) {
+    //TODO 19: Create a variable to change the theme
     return Scaffold(
       appBar: AppBar(
         title: const Text('Shared Preferences Demo'),
@@ -24,12 +42,17 @@ class _HomeScreenState extends State<HomeScreen> {
                   'Dark Mode',
                   style: TextStyle(fontSize: 20),
                 ),
-                Switch.adaptive(value: false, onChanged: null),
+                Switch.adaptive(
+                    //TODO 21: Change the value properity
+                    value: false,
+                    //TODO 22: callback the method to change the theme
+                    onChanged: null),
               ],
             ),
             Padding(
               padding: const EdgeInsets.all(24.0),
               child: TextField(
+                //TODO 3: Access to User's Text by Controller
                 decoration: const InputDecoration(
                   hintText: 'Email',
                   hintStyle: TextStyle(fontSize: 20),
@@ -44,7 +67,11 @@ class _HomeScreenState extends State<HomeScreen> {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    //TODO 5: Call the Method to save the data
+
+                    //TODO 6: Clear Text Field
+                  },
                   child: const Text('Save Data'),
                 ),
                 ElevatedButton(
@@ -63,6 +90,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                       'You Entered this Email:',
                                       style: TextStyle(fontSize: 15),
                                     ),
+                                    //TODO 10: Show User Email in Text widget
                                     Text(''),
                                     ElevatedButton(
                                       onPressed: () => Navigator.pop(context),
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'home_screen.dart';
 
@@ -14,11 +15,26 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  //TODO 13: Create insatance of themeData
+
+  //TODO 11: Create lightTheme using ThemeData class
+
+  //TODO 12: Create darkTheme using ThemeData class
+
+  //TODO 15: Create a method to load the theme
+
+  //TODO 17: Create a method to change the Theme
+
+  //TODO 16: Call the initState method
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      //TODO 14: Chanage theme properity with the instance
+
       theme: ThemeData.light(),
+      //TODO 22: modify the Home Screen
       home: HomeScreen(),
     );
   }
